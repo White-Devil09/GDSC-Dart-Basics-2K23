bca83cfb7e0b463fc72782fecb9464a2199557bd
Bhanu <bhanujarpula9@gmail.com>
2023-02-18 13:04:59 +0530

    Switch case

---
+++ 06_Switch_case.dart
@@ -0,0 +1,21 @@
+void main(){
+  String grade = 'A';
+
+  // Switch case is allowed only for 'int' or 'String'
+  switch(grade){
+    case 'A':
+      print("Your grade is $grade");
+      break;
+    case 'B':
+      print("Your grade is $grade");
+      break;
+    case 'C':
+      print("Your grade is $grade");
+      break;
+    case 'D':
+      print("Your grade is $grade");
+      break;
+    default:
+      print("Sorry you failed!");
+  }
+}
